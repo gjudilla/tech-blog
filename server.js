42fd0914ad0092d17406b899090efa1956da4938
gjudilla <gjudilla@gmail.com>
2024-05-01 21:28:01 -0700

    set up server

--- server.js
+++ server.js
@@ -7,10 +7,33 @@ const routes = require ('./controllers');
 const helpers = require ('./utils/helpers');
 const sequelize = require ('./config/connection');
 
+// Express/PORT set up
 const app = express();
 const PORT = process.env.PORT;
 
-// FINISH SET UP
+// Set up session
+const sess = {
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 900000, 
+    }
+};
+app.use(session(sess));
+
+// Set up handlebars engine
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}.`));
